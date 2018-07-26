e1c641eeb4e9c8ef131265e34e85cc982eb8e173
Paul <paulageyev@gmail.com>
2018-07-26 16:01:59 +0300

    Create a model `car`

---
+++ app/models/car.js
@@ -0,0 +1,17 @@
+export default DS.Model.extend({
+  title:  DS.attr(),
+  brand:  DS.attr(),
+  model:  DS.attr(),
+  engine:  DS.attr(),
+  power:  DS.attr(),
+  doors:  DS.attr(),
+  colors:  DS.attr(),
+  drive_wheel:  DS.attr(),
+  category: DS.attr(),
+  price: DS.attr(),
+  quantity: DS.attr(),
+  city:  DS.attr(),
+  address:  DS.attr(),
+  image:  DS.attr(),
+  description:  DS.attr(),
+});
